42aec0f1f915e0edf3d89e308f6edfe8748f4155
LukeyBeachBoy <lb580@kent.ac.uk>
2018-11-19 18:23:41 +0000

    Refactored mongoose and added POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
+var app = express();
+app.use(bodyParser.json());
+/* 
+The bodyParser.json() middleware we are 
+using stores the posted data, parsed as an object
+in the request variable
+*/
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.post('/users', (req, res) => {
+    var user = new User({
+        email: req.body.email
+    });
+
+    user.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
